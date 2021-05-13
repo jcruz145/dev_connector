f1ebbc0c463a4ada16c142917a640a7ad1ab41db
Jan Cruz <jcruz145@gmail.com>
2021-05-13 13:21:53 -0400

    prettify all code

--- config/db.js
+++ config/db.js
@@ -3,14 +3,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    console.log('connectDB Initialized');
-    try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-        console.log('MongoDB Connected...');
-    } catch(err) {
-        console.error(`MongoDB Failed Connect with error: ${err.message}`);
-        process.exit(1);
-    }
-}
+  console.log('connectDB Initialized');
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(`MongoDB Failed Connect with error: ${err.message}`);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,72 +12,75 @@ const { check, validationResult } = require('express-validator');
 // @access Public
 
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch (err) {
-        res.status(500).send('Server error');
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
 });
 
 // @route  POST api/auth
 // @desc   Authenticate user and get token
 // @access Public
 
-router.post('/', [
+router.post(
+  '/',
+  [
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-], async (req, res) => {
-
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
 
     try {
+      // See if user exists
+      let user = await User.findOne({ email });
 
-        // See if user exists
-        let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
 
-        if(!user) {
-           return res.status(400).json({ errors: [{ msg:'Invalid Credentials' }] });
+      // Match Email and Password
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return JWT
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-
-        // Match Email and Password
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch) {
-            return res.status(400).json({ errors: [{ msg:'Invalid Credentials' }] });
-        }
-
-        // Return JWT
-        
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            { expiresIn: 360000  },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            }
-            );
-
-
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
-
-});
+  }
+);
 
 module.exports = router;
